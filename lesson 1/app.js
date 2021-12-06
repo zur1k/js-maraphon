2c9c675a5b25fa42732290f711824ef97a53bd1b
zur1k <zazamuradov@gmail.com>
2021-12-06 21:31:56 +0200

    lesson 2

--- lesson 1/app.js
+++ lesson 1/app.js
@@ -1,15 +1,20 @@
-const slides = document.querySelectorAll('.slide')
+function pluginSlider(activeSlide = 1) {
+	const slides = document.querySelectorAll('.slide')
+	slides[activeSlide].classList.add('active')
 
-slides.forEach((slide) => {
-	slide.addEventListener('click', () => {
-		clearActiveClasses()
-
-		slide.classList.add('active')
-	})
-})
-
-function clearActiveClasses() {
 	slides.forEach((slide) => {
-		slide.classList.remove('active')
+		slide.addEventListener('click', () => {
+			clearActiveClasses()
+
+			slide.classList.add('active')
+		})
 	})
+
+	function clearActiveClasses() {
+		slides.forEach((slide) => {
+			slide.classList.remove('active')
+		})
+	}
 }
+
+pluginSlider()
--- lesson 2/app.js
+++ lesson 2/app.js
@@ -1,15 +1,45 @@
-const slides = document.querySelectorAll('.slide')
+const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
 
-slides.forEach((slide) => {
-	slide.addEventListener('click', () => {
-		clearActiveClasses()
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragend', dragend)
 
-		slide.classList.add('active')
-	})
-})
+for (const placeholder of placeholders) {
+	placeholder.addEventListener('dragover', dragover)
+	placeholder.addEventListener('dragenter', dragenter)
+	placeholder.addEventListener('dragleave', dragleave)
+	placeholder.addEventListener('drop', dragdrop)
+}
 
-function clearActiveClasses() {
-	slides.forEach((slide) => {
-		slide.classList.remove('active')
-	})
+function dragstart(event) {
+	event.target.classList.add('hold')
+	setTimeout(() => {
+		event.target.classList.add('hide')
+	}, 0);
+}
+
+
+function dragend(event) {
+	event.target.className = 'item'
+}
+
+function dragover(event) {
+	event.preventDefault()
+}
+
+function dragenter(event) {
+	event.target.classList.add('hovered')
+	console.log('drag enter')
+
+
+}
+function dragleave(event) {
+	event.target.classList.remove('hovered')
+	console.log('drag leave')
+
+
+}
+function dragdrop(event) {
+	event.target.classList.remove('hovered')
+	event.target.append(item)
 }
